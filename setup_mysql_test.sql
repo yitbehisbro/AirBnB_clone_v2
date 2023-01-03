24aa87647d642bca20911855b5efb13fe401cf18
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-03 12:34:13 +0300

    Some updates to the setup_mysql_test.sql

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -5,4 +5,3 @@ CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED WITH mysql_native_password BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
 GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost' WITH GRANT OPTION;
-
