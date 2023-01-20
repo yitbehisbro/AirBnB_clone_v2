17e72d8f2812350b6d98f293ff57fc56fc2230cd
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-20 15:58:36 +0000

    Prepare my web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -13,8 +13,9 @@ sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/current
 echo "The NGINX SOFTWARE installed successfully!!" > /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R "$USER" /data/
 sudo chown "$USER" /etc/nginx/sites-enabled/default
 sudo echo -e "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\tlocation / {\n\t\troot /data/web_static/releases/test/;\n\t}\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n}" > /etc/nginx/sites-enabled/default
