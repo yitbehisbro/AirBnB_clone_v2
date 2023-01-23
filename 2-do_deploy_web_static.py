a8754aa32ce20fd8451dade43a5f1eb91f8bcec4
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-01-23 18:16:36 +0300

    Updates

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,31 +1,32 @@
 #!/usr/bin/python3
-""" Deploy archive to the both servers """
+""" Function that compress a folder """
 from datetime import datetime
-import os
-import shlex
 from fabric.api import *
+import shlex
+import os
 
 
 env.hosts = ['54.237.120.163', '52.91.101.243']
 env.user = "ubuntu"
 
+
 def do_deploy(archive_path):
-    """ fab function to deploy archived file to the servers """
+    """ Deploys """
     if not os.path.exists(archive_path):
         return False
     try:
-        filename1 = archive_path.replace('/', ' ')
-        filename1 = shlex.split(filename1)
-        filename1 = filename1[-1]
+        name = archive_path.replace('/', ' ')
+        name = shlex.split(name)
+        name = name[-1]
 
-        filename2 = shlex.split(archive_path.replace('/', ' '))
-        filename2 = shlex.split(filename2[1].replace('.', ' '))
-        filename2 = filename2[0]
+        wname = name.replace('.', ' ')
+        wname = shlex.split(wname)
+        wname = wname[0]
 
-        releases_path = "/data/web_static/releases/{}/".format(filename2)
-        tmp_path = "/tmp/{}".format(filename1)
+        releases_path = "/data/web_static/releases/{}/".format(wname)
+        tmp_path = "/tmp/{}".format(name)
 
-        put(archive_path, "/tmp/", preserve_mode=True)
+        put(archive_path, "/tmp/")
         run("mkdir -p {}".format(releases_path))
         run("tar -xzf {} -C {}".format(tmp_path, releases_path))
         run("rm {}".format(tmp_path))
@@ -35,5 +36,5 @@ def do_deploy(archive_path):
         run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except Exception:
+    except:
         return False
