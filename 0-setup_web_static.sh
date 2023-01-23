bba0c41d3b5ae69bf39038318300e1e74bbe8ec0
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-23 08:20:00 +0000

    Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -2,16 +2,18 @@
 #Configurations for ngnix to deploy AirBnB project
 sudo apt update
 sudo apt -y install nginx
-sudo service nginx start
 sudo mkdir -p /data/
 sudo mkdir -p /data/web_static/
 sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/current
-echo "The NGINX SOFTWARE installed successfully!!" > /data/web_static/releases/test/index.html
-sudo ln -s /data/web_static/current /data/web_static/releases/test/
-sudo chown -R "$USER" /data/
+# sudo mkdir -p /data/web_static/current
+sudo touch /data/web_static/releases/test/index.html
+sudo chown "$USER" /data/web_static/releases/test/index.html
+sudo echo "The NGINX SOFTWARE installed successfully!!" | sudo tee -i /data/web_static/releases/test/index.html
+# sudo ln -sf /data/web_static/current /data/web_static/releases/test/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R "ubuntu" /data/
 sudo chown "$USER" /etc/nginx/sites-enabled/default
-echo -e "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\tlocation / {\n\t\troot /data/web_static/releases/test/;\n\t}\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n}" | sudo tee /etc/nginx/sites-enabled/default
+sudo echo -e "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\tlocation / {\n\t\troot /data/web_static/releases/test/;\n\t}\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n}" | sudo tee -i /etc/nginx/sites-enabled/default > /dev/null 2>&1
 sudo service nginx restart
