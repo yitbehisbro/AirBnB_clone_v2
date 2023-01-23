975ee412dba04ff5f78e7a91009f376e2fc7601b
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-01-23 12:58:16 +0300

    Updates

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,7 +14,7 @@ sudo chown "$USER" /data/web_static/releases/test/index.html
 sudo echo "The NGINX SOFTWARE installed successfully!!" | sudo tee -i /data/web_static/releases/test/index.html > /dev/null 2>&1
 # sudo ln -sf /data/web_static/current /data/web_static/releases/test/
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R "ubuntu" /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo chown "$USER" /etc/nginx/sites-enabled/default
 sudo echo -e "server {\n\tlisten 80 default_server;\n\tlisten [::]:80 default_server;\n\n\tlocation / {\n\t\troot /data/web_static/releases/test/;\n\t}\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n}" | sudo tee -i /etc/nginx/sites-enabled/default > /dev/null 2>&1
 sudo service nginx restart
