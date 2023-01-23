689cb62e5f2a73602159e65152a8e3cee14e9622
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-01-23 10:14:50 +0000

    Compress before sending web_static folder

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Compress before sending web_static folder """
+from datetime import datetime
+from fabric.api import local
+import os
+
+
+def do_pack():
+    try:
+        if not os.path.exists("versions"):
+            local('mkdir versions')
+        t = datetime.now()
+        f = "%Y%m%d%H%M%S"
+        archived_file = 'versions/web_static_{}.tgz'.format(t.strftime(f))
+        local('tar -cvzf {} web_static'.format(archived_file))
+        return archived_file
+    except Exception:
+        return None
