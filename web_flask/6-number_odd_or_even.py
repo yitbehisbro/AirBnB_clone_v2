9b7c9a76acafbcffdca2e6008aa453c511b1f12e
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-02-03 15:06:20 +0300

    Checks if the passed route variable odd or even and render the text in the html body

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+""" Run the first flask """
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Return the text """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def do_hbnb():
+    """ Displays HBNB """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def do_with_variable(text):
+    """ Displays with variable """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route('/python', defaults={'text': 'is cool'})
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<string:text>', strict_slashes=False)
+def do_with_variable2(text):
+    """ Displays with variable and path handling """
+    return "Python {}".format(text.replace("_", " "))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def do_with_variable3(n):
+    """ Displays the text if the path variable is integer """
+    text = "{} is a number".format(n)
+    return text
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def do_with_variable4(n):
+    """ Displays the text if the path variable is integer """
+    text = "Number: {}".format(n)
+    return render_template('5-number.html', text=text)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def do_with_variable5(n):
+    """ Displays the text if the path variable is integer
+    and checks if it is odd or even """
+    text = ""
+    if n % 2 == 0:
+        text = "Number: {} is even".format(n)
+    else:
+        text = "Number: {} is odd".format(n)
+    return render_template('6-number_odd_or_even.html', text=text)
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
