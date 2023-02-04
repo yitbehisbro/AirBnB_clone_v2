973de3656192e91fb608511a8dd36c1fe3a9015b
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-04 17:17:47 +0300

    Error fixed!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" Run the first flask """
+""" Run the first flask application """
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -12,4 +13,5 @@ def hello_hbnb():
 
 
 if __name__ == "__main__":
+    """ Run the first flask application """
     app.run(host='0.0.0.0', port=5000)
