f80208701581deec0de5bbfc4caa2e104129ebfd
yitbehisbro <yitbarekw@hu.edu.et>
2023-02-04 17:24:01 +0300

    COntent added!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Return the text """
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
 if __name__ == "__main__":
